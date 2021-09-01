e893da5a1a56abd012c4bbaa938ea8f75a398b09
Jakub Bednarek <jakub.bednarek@fresha.com>
2021-09-01 13:43:44 +0200

    Cleanup example

--- examples/helloworld/priv/status_details.exs
+++ examples/helloworld/priv/status_details.exs
@@ -1,7 +1,6 @@
 {:ok, channel} = GRPC.Stub.connect("localhost:50051", interceptors: [GRPC.Logger.Client])
 
-{:error, reason, headers} =
-  channel |> Helloworld.Greeter.Stub.say_hello(Helloworld.HelloRequest.new(name: "quota_failure:word"), return_headers: true)
-  |> IO.inspect(label: "[#{__ENV__.file}:#{__ENV__.line}] debug print")
+{:error, reason} =
+  channel |> Helloworld.Greeter.Stub.say_hello(Helloworld.HelloRequest.new(name: "quota_failure:word"))
 
-# IO.inspect(reply)
+IO.inspect(reason)
