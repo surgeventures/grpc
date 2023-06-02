b45b2a68a686ddf7208f6f6a8f823b5821023ac2
Kamil Kowalski <kamil@fresha.com>
2023-06-02 19:07:34 +0200

    Lock protobuf to < 0.10.0 when breaking changes were introduced

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule GRPC.Mixfile do
 
   defp deps do
     [
-      {:protobuf, "~> 0.5"},
+      {:protobuf, ">= 0.5.0 and < 0.10.0"},
       {:cowboy, "~> 2.7"},
       {:gun, "~> 2.0"},
       {:ex_doc, "~> 0.23", only: :dev},
